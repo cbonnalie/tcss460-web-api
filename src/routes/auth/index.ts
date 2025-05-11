0dd393020db3c4afa1bc3d97dd9731234c03f000
kalebanagnostou <anagnostou.kaleb@gmail.com>
2025-05-10 20:04:37 -0700

    delete_user routes

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -3,9 +3,15 @@ import express, { Router } from 'express';
 import { signinRouter } from './login';
 import { registerRouter } from './register';
 import { changePassRouter } from './changepass';
+import { deleteUserRouter } from './delete_user';
 
 const authRoutes: Router = express.Router();
 
-authRoutes.use(signinRouter, registerRouter, changePassRouter);
+authRoutes.use(
+    signinRouter,
+    registerRouter,
+    changePassRouter,
+    deleteUserRouter
+);
 
 export { authRoutes };
